70b4760f061c6b9bb5119cb7175fbed66d98a84a
Tyler <tylerka13514007@gmail.com>
2026-02-07 02:29:50 +0800

    feat(history): show recent 10 on home and add full history screen

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AddHandScreen from '../screens/AddHandScreen';
 import GameDashboardScreen from '../screens/GameDashboardScreen';
 import HomeScreen from '../screens/HomeScreen';
+import HistoryScreen from '../screens/HistoryScreen';
 import NewGameStepperScreen from '../screens/NewGameStepperScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import SummaryScreen from '../screens/SummaryScreen';
@@ -20,6 +21,7 @@ function RootNavigator() {
       }}
     >
       <Stack.Screen name="Home" component={HomeScreen} options={{ title: t('nav.home') }} />
+      <Stack.Screen name="History" component={HistoryScreen} options={{ title: t('nav.history') }} />
       <Stack.Screen
         name="NewGameStepper"
         component={NewGameStepperScreen}

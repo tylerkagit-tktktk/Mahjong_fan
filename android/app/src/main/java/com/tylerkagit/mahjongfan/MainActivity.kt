8caac6ff77594b44b576119f82cbbe20e3883388
Tyler <tylerka13514007@gmail.com>
2026-02-24 03:30:10 +0800

    chore(branding): rename app to MahjongFan and rebuild Android launcher icons

--- android/app/src/main/java/com/tylerkagit/mahjongfan/MainActivity.kt
+++ android/app/src/main/java/com/tylerkagit/mahjongfan/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "mahjong_be_fd"
+  override fun getMainComponentName(): String = "MahjongFan"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
